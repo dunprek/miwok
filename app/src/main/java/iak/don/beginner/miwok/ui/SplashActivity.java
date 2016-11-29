32f685a6c6250fd48a7f1a365883420fc0413d9c
GIDEON\don <gideon@ruangguru.com>
2016-11-29 08:43:32 +0700

    fix

--- app/src/main/java/iak/don/beginner/miwok/ui/SplashActivity.java
+++ app/src/main/java/iak/don/beginner/miwok/ui/SplashActivity.java
@@ -1,5 +1,6 @@
 package iak.don.beginner.miwok.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,28 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3 * 1000);
+
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
     }
 }
